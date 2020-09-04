c931725ff2bf996ffb97519d859f3a95524272e0
Laerson Castro <laersonjr@gmail.com>
2020-09-04 16:57:49 -0300

    Modulo 5 estrutura de desição

---
+++ src/algaworks/fund/java/mod/cinco/VerificarConcurso.java
@@ -0,0 +1,38 @@
+package algaworks.fund.java.mod.cinco;
+
+import java.util.Scanner;
+
+public class VerificarConcurso {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		Scanner sc = new Scanner(System.in);
+		
+		final Double notaMinima = 60.0;
+		
+		final Double mediaMinima = 150.0;
+		
+		System.out.printf("Digite a nota da prova de português: ");
+		Double notaPort = sc.nextDouble();
+		
+		System.out.printf("Digite a nota da prova de matemática: ");
+		Double notaMat = sc.nextDouble();
+		
+		Double media = notaMat + notaPort;
+		
+		Boolean validarNotaMinima = notaPort < notaMinima || notaMat < notaMinima;
+		Boolean validarMediaMinima = mediaMinima > media;
+		
+		if (validarNotaMinima) {
+			System.out.println("Reprovado.");
+		} else if (validarMediaMinima) {
+			System.out.println("Reprovado.");
+		} else {
+			System.out.println("Aprovado.");
+		}
+		
+		
+	}
+
+}
